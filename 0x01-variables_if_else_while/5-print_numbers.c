1ee63e67dce8dc80a644003f1e4dbd9d3aaa135f
Orlando amarteifio <seniorcorradi1234@gmail.com>
2022-09-09 20:29:18 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include < stdio.h>
+#include <stdib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0.
+*/
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+printf("%d", num);
+
+printf("\n");
+
+return (0);
+
+}
