f079b19ffe920e86029ac252ac3f16b0d23e19c9
Orlando amarteifio <seniorcorradi1234@gmail.com>
2022-09-14 20:15:29 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_alphaber_x10 - function to print abc 1o times
+ * print_alphaber_x10 - function to print abc 10 times
  *
  * Return: 0
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 char c, i;
 
