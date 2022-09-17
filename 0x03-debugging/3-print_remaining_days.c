233f67b4c3f59302642cecc67fa2ee7d621db98b
Orlando amarteifio <seniorcorradi1234@gmail.com>
2022-09-17 10:26:01 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -26,7 +26,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
@@ -35,4 +35,3 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
-  
