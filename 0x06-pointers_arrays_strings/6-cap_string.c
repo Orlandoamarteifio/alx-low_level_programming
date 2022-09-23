45150ee840e26556442d3e545640c11bded7a3b1
Orlando amarteifio <seniorcorradi1234@gmail.com>
2022-09-22 20:46:38 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,37 @@
 #include "main.h"
-
-
-
 /**
-
- * string_toupper - a function that changes all lowercase letters of a  string
-
- * to upper case
-
- * @s: an input string
-
- * Return: char pointer to converted string
-
+*cap_string - capitalizes every first letter of a word in a string.
+*separators of words are:  space, tabulation,
+  * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@x: pointer to string.
+ *
+ *Return: pointer to x.
  */
-
-char *string_toupper(char *s)
-  
+char *cap_string(char *x)
 {
-  
-  char *start = s;
-  
+int count;
 
-  
-  while (*s)
-    
-    {
-      
-      if (*s >= 'a' && *s <= 'z')
-	
-	*s -= 32;
-      
-      s++;
-      
-    }
-  
-  return (start);
-  
+/*  scan through string */
+count = 0;
+while (x[count] != '\0')
+{/* if next character after count is a char , capitalise it */
+if (x[0] >= 97 && x[0] <= 122)
+{
+x[0] = x[0] - 32;
 }
+if (x[count] == ' ' || x[count] == '\t' || x[count] == '\n'
+|| x[count] == ',' || x[count] == ';' || x[count] == '.'
+|| x[count] == '.' || x[count] == '!' || x[count] == '?'
+|| x[count] == '"' || x[count] == '(' || x[count] == ')'
+|| x[count] == '{' || x[count] == '}')
+{
+if (x[count + 1] >= 97 && x[count + 1] <= 122)
+{
+x[count + 1] = x[count + 1] - 32;
+}
+}
+count++;
+}
+return (x);
+}
+
