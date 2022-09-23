76a803f995cf2a6eaf5e1046db613afa27b8a49e
Orlando amarteifio <seniorcorradi1234@gmail.com>
2022-09-22 21:05:13 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -23,9 +23,9 @@ while (s[stringCount] != '\0')
 {
 leetCount = 0;
 while (leetCount < 10)
-{	  
+{
 if (leetLetters[leetCount] == s[stringCount])
-{	      
+{
 s[stringCount] = leetNums[leetCount];
 }
 leetCount++;
