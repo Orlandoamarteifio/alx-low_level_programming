9f5cc042baa2f31506f397dc38a96ff1fc2f23eb
Orlando amarteifio <seniorcorradi1234@gmail.com>
2022-10-31 13:26:28 -0700

    task 4

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: count
+ * @argv: arguments
+ * Return: Always 0.
+ */
+
+int main (int argc, char argv[])
+{
+
+}
